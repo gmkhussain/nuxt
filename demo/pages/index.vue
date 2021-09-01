a475daead6f2a7a75ba77a076ec76886b746c0b6
hussain <gmk.hussain@gmail.com>
2021-09-01 15:40:40 +0500

    init

---
+++ demo/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h4>{{pageTitle}}</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+    data() {
+        return {
+            pageTitle: "Home"
+        }
+    }
+}
+</script>
